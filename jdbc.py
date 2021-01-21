58f7a0f1e1365b4b24bd5fd3ef9f8a8cb84d2d32
Flpp <flpp.bkm@gmail.com>
2021-01-20 21:09:28 -0300

    jdbc

---
+++ jdbc.py
@@ -0,0 +1,18 @@
+"""host:ora_adprd.dc.nova
+port:1521
+database:ADPRD
+username:SRVDATA
+pwd:b1gd4t###drc2018
+"""
+url = "jdbc:oracle:thin:@ora_adprd.dc.nova:1521:ADPRD"
+properties = {"user" : "SRVDATA", "password" : "b1gd4t###drc2018"}
+
+df = (
+    spark.read.format("jdbc")
+        .option("url", url)
+        .option("driver", "oracle.jdbc.OracleDriver")
+        .option("dbtable", "ac_admin.ecad_order_mis")
+        .option("properties", properties)
+        .load()
+)
+
